3c73019429b25b41884d14d0dab13c4eadb3f6f3
Tanu Priya <tanu.25scs1003004493@iilm.edu>
2026-07-17 20:30:22 +0530

    Day 5 - Java DSA

---
+++ LargestElements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LargestElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int largest = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("The largest element is: " + largest);
+        scanner.close();
+    }
+}
+
+
